e2afb0d2463ab91c5c4bca1932281f553fbc1ed3
Matt Wilson <matthew@synesis.com.au>
2019-05-05 18:40:45 +1000

    - removed unnecessary using statements

--- test/scratch/Test.Scratch.CLASP.BoundStructure.2/Properties/AssemblyInfo.cs
+++ test/scratch/Test.Scratch.CLASP.BoundStructure.2/Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Test.Scratch.CLASP.BoundStructure.2")]
