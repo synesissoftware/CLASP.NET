c79248b8e541b4e39d86ccd1347501c2a4bca300
Matt Wilson <matthew@synesis.com.au>
2019-05-18 17:12:41 +1000

    + added CLASP.Util

--- src/CLASP.Util/Properties/AssemblyInfo.cs
+++ src/CLASP.Util/Properties/AssemblyInfo.cs
@@ -1,22 +1,28 @@
 ﻿
+// Created: 10th August 2009
+// Updated: 18th May 2019
+
 #pragma warning disable 1607
 
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Test.Scratch.ToolMainVA")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CLASP.Util")]
+[assembly: AssemblyDescription("CLASP.NET core API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Synesis Software Pty Ltd")]
 [assembly: AssemblyProduct("CLASP.NET")]
-[assembly: AssemblyCopyright("Copyright © Synesis Software Pty Ltd 2017-2019")]
+[assembly: AssemblyCopyright("Copyright © Synesis Software Pty Ltd 2009-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
+[assembly: AssemblyVersion("0.21.148.0")]
+[assembly: AssemblyFileVersion("0.21.148.0")]
 [assembly: AssemblyInformationalVersion("0.21")]
 
 /* ///////////////////////////// end of file //////////////////////////// */
+
